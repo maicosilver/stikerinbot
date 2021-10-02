91f1b490dee26c3bf5e7a8e5b595282c542d4950
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-10-01 21:22:43 -0300

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -37,6 +37,7 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^wm$/i
+handler.owner = true
 
 handler.limit = true
 
