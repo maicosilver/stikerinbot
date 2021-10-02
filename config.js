1422e86676a8851387f54d73b7072a7c1c6975de
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-10-01 21:12:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012', '6288709645535'] // Letakan nomor kamu disini
+global.owner = ['558896724995', '994406932013'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = '❤️'
-global.author = 'ariffb'
+global.packname = 'nokia'
+global.author = '9672-4995'
 
 global.wait = '_*tunggu..*_'
 global.eror = '_*Server Error*_'
